025c3ee782f9603f430fe4860f359845e50edff7
kozy10 <kentaro1002vb@gmail.com>
2018-07-25 22:03:20 +0900

    Fisish correcting

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,5 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
+
+Refile.secret_key = '1e69f221d6dd56473657c7a0abe5aadba64653713ebda96e44e48162b886fd2edf43e7770c0b566287a99a5a8d161e706c495ef480ccf3d8516131e27003fb9a'
 # ActiveSupport::Reloader.to_prepare do
 #   ApplicationController.renderer.defaults.merge!(
 #     http_host: 'example.org',
